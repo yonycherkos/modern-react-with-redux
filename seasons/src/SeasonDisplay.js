64d492670fe164eee39863b800e2463f45b4ec0f
yonathan <yonycherkos@gmail.com>
2019-12-03 11:10:06 +0300

    Structuring Apps with Class-Based Components

--- seasons/src/SeasonDisplay.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DisplaySeason = () => {
-  return <div>display season</div>;
-};
-
-export default DisplaySeason;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,26 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: "" };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-
   render() {
-    if (this.state.lat && !this.state.errorMessage) {
-      return <div>latitude: {this.state.lat}</div>;
-    } else if (!this.state.lat && this.state.errorMessage) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else {
-      return <div>Loading!</div>;
-    }
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>latitude: </div>;
   }
 }
 
